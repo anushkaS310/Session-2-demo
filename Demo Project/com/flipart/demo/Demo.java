5324d32620597f2f5e831e5f37d19c125200709c
Anushka Srivastava <anushka.srivastava@BLRETV-N173G8WN.local>
2022-06-07 08:50:58 +0530

    java program

---
+++ Demo Project/com/flipart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipart.demo;
+
+/**
+ * @author anushka.srivastava
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
